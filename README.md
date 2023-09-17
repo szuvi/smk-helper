3e665ef93e3205c9208abf88ae772e4ca9625510
Konrad Trzciński <32699655+szuvi@users.noreply.github.com>
2023-09-17 13:16:34 +0200

    Feature/linter
    * Add linting, formatting and precommit
    * Initial linting

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # SMK Helper
 
 ## Development instructions
+
 1. `git clone https://github.com/szuvi/smk-helper.git`
 2. `cd smk-helper`
 3. `npm install`
 4. `npm run dev`
-***
+
+---
+
 ## [Original Setup Instructions](https://github.com/electron-vite/electron-vite-react)
--- src/components/update/Progress/index.tsx
+++ src/components/update/Progress/index.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import styles from './progress.module.scss'
+import styles from './progress.module.scss';
 
-const Progress: React.FC<React.PropsWithChildren<{
-  percent?: number
-}>> = props => {
-  const { percent = 0 } = props
+import React from 'react';
+
+const Progress: React.FC<
+  React.PropsWithChildren<{
+    percent?: number;
+  }>
+> = (props) => {
+  const { percent = 0 } = props;
 
   return (
     <div className={styles.progress}>
-      <div className='progress-pr'>
-        <div
-          className='progress-rate'
-          style={{ width: `${percent}%` }}
-        />
+      <div className="progress-pr">
+        <div className="progress-rate" style={{ width: `${percent}%` }} />
       </div>
-      <span className='progress-num'>{(percent ?? 0).toString().substring(0,4)}%</span>
+      <span className="progress-num">{(percent ?? 0).toString().substring(0, 4)}%</span>
     </div>
-  )
-}
+  );
+};
 
-export default Progress
+export default Progress;
