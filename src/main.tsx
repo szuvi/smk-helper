860753deeaa3e11903906dcb335dad9d18eadb23
Konrad Trzciński <konrad@trzcinski.work>
2023-09-17 13:15:33 +0200

    Initial linting

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './samples/node-api'
-import './index.scss'
+import App from './App';
+import './index.scss';
+import './samples/node-api';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
 
-postMessage({ payload: 'removeLoading' }, '*')
+postMessage({ payload: 'removeLoading' }, '*');
